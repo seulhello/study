3703463f426c175552fc2e25c515aeae712bd145
siri <ysh7731@gmail.com>
2019-04-26 17:26:36 +0900

    Updates

---
+++ precache-manifest.4c15de2ecfcf660428a21b603ad71368.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "10d4ef04297f48e557c3c25ff84f9829",
+    "url": "/movie_app/index.html"
+  },
+  {
+    "revision": "dc595cd4ab75b4560cfa",
+    "url": "/movie_app/static/css/main.7bcb57c4.chunk.css"
+  },
+  {
+    "revision": "9cb69e99b275b59305e2",
+    "url": "/movie_app/static/js/2.66c7f579.chunk.js"
+  },
+  {
+    "revision": "dc595cd4ab75b4560cfa",
+    "url": "/movie_app/static/js/main.5d9ef43c.chunk.js"
+  },
+  {
+    "revision": "c6514544d436775abdc2",
+    "url": "/movie_app/static/js/runtime~main.175dda75.js"
+  }
+]);
